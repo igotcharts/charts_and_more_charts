5662d4ae8cb8a0bb8e0371e070f55c0be79f122a
I Got Charts <alex.pudlin@lacity.org>
2017-08-31 22:08:49 -0700

    semi complete version of lil chart

---
+++ functions/cool_random_functions.py
@@ -0,0 +1,10 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+def RGBToHTMLColor(rgb_tuple):
+    """ convert an (R, G, B) tuple to #RRGGBB """
+    hexcolor = '#%02x%02x%02x' % rgb_tuple
+    return hexcolor
+
