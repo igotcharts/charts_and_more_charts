601ded0105fd88ca3d646f2f414cb12250a0bfa3
I Got Charts <alex.pudlin@lacity.org>
2017-09-14 23:23:18 -0700

    simple chart of monuments

--- functions/charts_function_list.py
+++ functions/charts_function_list.py
@@ -16,8 +16,8 @@ import string
 import random
 
 def folder_setup():
-    current_dir = os.getcwd()
-    os.chdir('..')
+    if os.path.split(os.getcwd())[1] == 'notebooks':
+        os.chdir('..')
     base_path = os.getcwd()
     data_folder = (os.path.join(base_path,'data'))
     output_folder = (os.path.join(base_path,'outputs'))
