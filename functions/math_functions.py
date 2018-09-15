68b0e324039a37f6e53d0661640d477087b3a352
I Got Charts <alex.pudlin@lacity.org>
2018-09-14 18:11:19 -0700

    added cereal chart

--- functions/math_functions.py
+++ functions/math_functions.py
@@ -6,3 +6,21 @@ def roundup(x):
 
 def percent_convert(df,column):
     return '%.1f'%df[column]+'%'
+
+def myround(x, base=5):
+    return int(base * round(float(x)/base))
+
+
+def percent_convert(x,percent_format):
+    """
+    Converts a series to percent text format
+
+    Parameters
+    ------------
+    "pecent_format": boolean. If series values are already in percent format, set as True. Otherwise set as false.
+
+    """
+    if percent_format == True:
+        return '%.1f'%(x*100)+'%'
+    else:
+        return '%.1f'%x+'%'
